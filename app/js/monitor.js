b27becada4f6de6a3c52422be34f3b8d861b81ef
alexnas <alexnas2python@gmail.com>
2020-06-17 13:42:47 +0300

    Show dinamic progress bar CPU overload.

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,11 +4,23 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+const cpuOverload = 80;
+
 // Run every 2 seconds
 setInterval(() => {
   // CPU Usage
   cpu.usage().then(info => {
+    console.log(info);
     document.getElementById('cpu-usage').innerText = info + '%';
+
+    document.getElementById('cpu-progress').style.width = info + '%';
+
+    // Make progress bar red if overload
+    if (info >= cpuOverload) {
+      document.getElementById('cpu-progress').style.background = 'red';
+    } else {
+      document.getElementById('cpu-progress').style.background = '#30c88b';
+    }
   });
 
   // CPU Free
