ace2e844b9310ff140977b24f892a27e9bfda4da
alexnas <alexnas2python@gmail.com>
2020-06-17 12:50:01 +0300

    Create dynamic system stats.

--- app/js/monitor.js
+++ app/js/monitor.js
@@ -4,6 +4,23 @@ const cpu = osu.cpu;
 const mem = osu.mem;
 const os = osu.os;
 
+// Run every 2 seconds
+setInterval(() => {
+  // CPU Usage
+  cpu.usage().then(info => {
+    document.getElementById('cpu-usage').innerText = info + '%';
+  });
+
+  // CPU Free
+  cpu.free().then(info => {
+    document.getElementById('cpu-free').innerText = info + '%';
+  });
+
+  // System Uptime
+  document.getElementById('sys-uptime').innerText = secondsToDhms(os.uptime());
+  // console.log(secondsToDhms(os.uptime()));
+}, 2000);
+
 // Set CPU model
 document.getElementById('cpu-model').innerText = cpu.model();
 
@@ -17,3 +34,14 @@ document.getElementById('os').innerText = `${os.type()} ${os.arch()}`;
 mem.info().then(info => {
   document.getElementById('memory-total').innerText = info.totalMemMb;
 });
+
+// Show days, hours, mins, seconds
+function secondsToDhms(seconds) {
+  seconds = +seconds;
+  d = Math.floor((seconds / 3600) * 24);
+  h = Math.floor((seconds % (3600 * 24)) / 3600);
+  m = Math.floor((seconds % 3600) / 60);
+  s = Math.floor(seconds % 60);
+
+  return `${d}d, ${h}h, ${m}m, ${s}s`;
+}
